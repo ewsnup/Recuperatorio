8e8412b1c757921a893b2a1067d6fdb5e42e51e5
Ludmila <ludmilaabriluni@gmail.com>
2025-07-01 12:55:51 -0300

    Add files via upload

---
+++ Recuperatorio AD/Listas.py
@@ -0,0 +1,10 @@
+estudiantes = ["Ana", "Bruno", "Carla", "Diego"]
+
+materias = ["Matemática", "Historia", "Biología"]
+
+notas = [
+    [9, 8 , 10],
+    [6, 7, 8],
+    [10, 10, 9],
+    [7, 6, 5]
+]
