a3069d3b6cc3bde45ea4730c6da836ea29b1c3f8
Ludmila <ludmilaabriluni@gmail.com>
2025-07-01 10:46:22 -0300

    Main.py

--- Recuperatorio AD/Main.py
+++ Recuperatorio AD/Main.py
@@ -1,16 +1,6 @@
 #Gestión de Calificaciones de Estudiantes con Lista/Array y Matriz en Python
-
-# Lista Estudiantes
-estudiantes = ["Ana", "Bruno", "Carla", "Diego"]
-materias = ["Matemática", "Historia", "Biología"]
-notas = [
-    [9, 8 , 10],
-    [6, 7, 8],
-    [10, 10, 9],
-    [7, 6, 5]
-]
-
-from Funciones_main import *
+from Funciones_Main import *
+from Listas import *
 
 abrir = True
 while abrir:
@@ -22,29 +12,27 @@ while abrir:
 5. Salir.""")
 
     opcion = input("\nSeleccione una opción: ")
-
+    
     match opcion:
         case "1":
-            print("")
+            print()
             mostrar_estudiantes(estudiantes, notas)
         case "2":
-            print("")
-            print("Ordenando por promedio de notas...")
+            print("\nOrdenando por promedio de notas...")
             datos = sacar_promedio(estudiantes, notas)
             datos_ordenados = ordenar_promedio(datos)
             for estudiante in datos_ordenados:
                 print(f"{estudiante[0]}: {estudiante[1]} -> {estudiante[2]}")
         case "3":
-            print("")
-            nombre = input("Ingrese el nombre del estudiante que desea buscar: ")
+            nombre = input("\nIngrese el nombre del estudiante que desea buscar: ")
             print(f"\nBuscando a {nombre}...")
             mostrar_calificaciones(estudiantes, notas, nombre)
         case "4":
-            print("")
-            calificacion = int(input("Ingrese la calificación: "))
-            resultado = buscar_por_calificacion(notas, estudiantes, materias, calificacion)
-            if resultado False:
-                print("No se ha encontrado a ningún estudiante con esta calificación.")
+            try:
+                calificacion = int(input("\nIngrese la calificación: "))
+                resultado = buscar_por_calificacion(notas, estudiantes, materias, calificacion)
+            except ValueError:
+                print("Ingreso inválido. Solo ingresar números.")
         case "5":
             print("\nSaliendo del programa...\n")
             abrir = False
