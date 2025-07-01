95ef3ce97e917869baf8bfa1deddf16315508556
Ludmila <ludmilaabriluni@gmail.com>
2025-07-01 12:55:09 -0300

    Funciones_Main.py

--- Recuperatorio AD/Funciones_Main.py
+++ Recuperatorio AD/Funciones_Main.py
@@ -1,10 +1,16 @@
 #1
-def mostrar_estudiantes(estudiantes:list, notas:list) -> list:
+def mostrar_estudiantes(estudiantes:list, notas:list):
+    '''
+    Muestra a cada estudiantes y sus notas correspondientes.
+    '''
     for i in range(len(estudiantes)):
         print(f"{estudiantes[i]} -> {notas[i]}")
 
 #2
 def sacar_promedio(estudiantes:list, notas:list) -> list:
+    '''
+    Calcula el promedio de notas para cada estudiante y devuelve una lista.
+    '''
     datos = []
 
     for i in range(len(estudiantes)):
@@ -17,6 +23,10 @@ def sacar_promedio(estudiantes:list, notas:list) -> list:
     return datos
 
 def ordenar_promedio(datos:list) -> list:
+    '''
+    Ordena la lista de estudiantes por promedio (de mayor a menor)
+    usando bubble sort.
+    '''
     for i in range(len(datos)-1):
         for j in range(i+1, len(datos)):
             if datos[i][1] < datos[j][1]:
@@ -28,7 +38,9 @@ def ordenar_promedio(datos:list) -> list:
 
 #3
 def encontrar_alumno(estudiantes: list, nombre: str) -> int:
-    """Busca un alumno y retorna su posición. Si no existe, retorna -1."""
+    '''
+    Busca un estudiante por nombre y devuelve su índice (o -1 si no existe).
+    '''
     posicion = -1  
     for i in range(len(estudiantes)):
         if estudiantes[i] == nombre:
@@ -37,6 +49,9 @@ def encontrar_alumno(estudiantes: list, nombre: str) -> int:
     return posicion  
 
 def mostrar_calificaciones(estudiantes: list, notas: list, nombre: str):
+    '''
+    Muestra las notas y el nombre del estudiante si existe.
+    '''
     posicion = encontrar_alumno(estudiantes, nombre)
     if posicion != -1:
         print(f"{nombre} -> {notas[posicion]}")
@@ -46,14 +61,18 @@ def mostrar_calificaciones(estudiantes: list, notas: list, nombre: str):
 #4
 
 def buscar_por_calificacion(notas:list, estudiantes:list, materias:list, calificacion:int):
+    '''
+    Busca estudiantes que tengan una calificación específica
+    en alguna materia y muestra las coincidencias.
+    '''
     encontrado = False
     for i in range(len(estudiantes)):
         for j in range(len(materias)):
             if notas[i][j] == calificacion:
                 print(f"Nota encontrada: {calificacion}, Estudiante: {estudiantes[i]}, Materia: {materias[j]}")
                 encontrado = True
-                
-    if not encontrado:
+
+    if encontrado == False:
         print("No se ha encontrado a ningún estudiante con esta calificación.")
 
                 
